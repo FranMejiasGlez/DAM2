2b4bfb892f4288f5ac09d60135c53a24d3c00ef7
fran gonzalez <fran.mejias.glez98@gmail.com>
2025-08-01 12:23:19 +0200

    asd

---
+++ GITPUSH EUSA.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+REM Change directory to the repository
+cd /d "C:\Users\DAM1\Desktop\CompartidaVB\DAM2"
+
+REM Add all changes to staging
+git add .
+
+SET /P commitMessage="Mensaje del commit: "
+REM Commit changes with a message
+git commit -m "%commitMessage%"
+
+REM Push changes to the remote repository
+git push
+
+REM Pause to see the output
+PAUSE
---
+++ GITPUSH PORTATIL.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+REM Change directory to the repository
+cd /d "C:\Users\frans\Desktop\CompartidaVB\DAM\DAM2"
+
+REM Add all changes to staging
+git add .
+
+SET /P commitMessage="Mensaje del commit: "
+REM Commit changes with a message
+git commit -m "%commitMessage%"
+
+REM Push changes to the remote repository
+git push
+
+REM Pause to see the output
+PAUSE
