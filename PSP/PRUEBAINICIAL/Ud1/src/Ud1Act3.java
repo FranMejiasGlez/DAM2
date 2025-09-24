c58ff37528eb8ed9014e81d6c08d94d8fc970c2f
fran33 <franciscomejias.24@campuscamara.es>
2025-09-24 14:46:11 +0200

    ACTIVIDADES

--- PSP/PRUEBAINICIAL/Ud1/src/Ud1Act3.java
+++ PSP/PRUEBAINICIAL/Ud1/src/Ud1Act3.java
@@ -17,16 +17,55 @@ import java.io.InputStreamReader;
 public class Ud1Act3 {
 
     public static void main(String[] args) {
-        File fichero;
-        BufferedReader teclado;
-        String nombre;
+        File directorio, fichero1, fichero2;
 
-        teclado = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Creando directorio: NUEVODIR");
+
+        directorio = new File("./src/", "NUEVODIR");
+        directorio.mkdir();
+
+        System.out.println("En: " + directorio.getPath());
+        System.out.println("Creando ficheros vacios..");
         try {
-            nombre = teclado.readLine();
+            new File("./src/NUEVODIR/", "fichero1vacio.txt").createNewFile();
+            new File("./src/NUEVODIR/", "fichero2vacio.txt").createNewFile();
         } catch (IOException ioe) {
-            System.out.println("Error de E/S");
+            System.out.println("Error E/S");
         }
+        System.out.println("Listando: ");
+        for (int i = 0; i < directorio.listFiles().length; i++) {
 
+            if (directorio.listFiles()[i].isDirectory()) {
+
+                System.out.println(directorio.listFiles()[i]);
+                System.out.println(" <dir>");
+
+            } else {
+                System.out.println(directorio.listFiles()[i]);
+                System.out.println("tamanio: "
+                        + directorio.listFiles()[i].length());
+            }
+        }
+        System.out.println("Renombrando fichero1vacio.txt a fich1...");
+        
+        File fich1 = new File("./src/NUEVODIR/fich1.txt");
+        
+        directorio.listFiles()[1].renameTo(fich1);
+        
+        System.out.println("Listando: ");
+        
+        for (int i = 0; i < directorio.listFiles().length; i++) {
+
+            if (directorio.listFiles()[i].isDirectory()) {
+
+                System.out.println(directorio.listFiles()[i]);
+                System.out.println(" <dir>");
+
+            } else {
+                System.out.println(directorio.listFiles()[i]);
+                System.out.println("tamanio: "
+                        + directorio.listFiles()[i].length());
+            }
+        }
     }
 }
