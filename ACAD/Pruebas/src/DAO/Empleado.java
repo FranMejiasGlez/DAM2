4e5a56bf39bb154ddf82b756de1615aba791a1c0
fran33 <franciscomejias.24@campuscamara.es>
2025-10-20 11:03:58 +0200

    aadd

---
+++ ACAD/Pruebas/src/DAO/Empleado.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author Mejias Gonzalez Francisco
+ */
+public class Empleado {
+
+    public int id;
+    public String nombre;
+    public String apellidos;
+    public float sueldo;
+    
+    public Empleado(){
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return the sueldo
+     */
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    /**
+     * @param sueldo the sueldo to set
+     */
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+    
+}
--- ACAD/Pruebas/src/PruebaFichDataFija.java
+++ ACAD/Pruebas/src/PruebaFichDataFija.java
@@ -1,10 +1,15 @@
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
@@ -18,7 +23,11 @@ import java.nio.charset.StandardCharsets;
  */
 public class PruebaFichDataFija {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        byte tamanioNombre = 30;
+        char caracterNombre;
+
+
         try {
             DataInputStream fichero = new DataInputStream(
                     new FileInputStream("./src/FichData.dat"));
@@ -26,18 +35,44 @@ public class PruebaFichDataFija {
             DataOutputStream fichero2 = new DataOutputStream(
                     new FileOutputStream("./src/FichDataFija.dat"));
 
-            //Leer Fichero
+            //Leer Fichero Variable y escribir en Fichero Fijo
+            System.out.println("Leyendo Fichero Variable y escribiendo en Fichero Fijo...");
             while (true) {
                 String nombre = fichero.readUTF();  // Lee nombre en UTF_16
-                fichero2.                      // Escribe nombre en char
+                StringBuilder nombreFijo;
+
+                nombreFijo = new StringBuilder(nombre);
+                nombreFijo.setLength(30); //Tamaño fijo de 30
+                nombre = nombreFijo.toString(); //Transformar para escribir
+                // System.out.println(nombre);
+                fichero2.writeChars(nombre);// Escribe nombre en char
                 int edad = fichero.readByte();      // Lee edad en byte
-                // Escribe edad
-                System.out.println("Nombre: " + nombre + ", Edad: " + edad);
+                fichero2.writeByte(edad);// Escribe edad
+                // System.out.println("Nombre: " + nombre + ", Edad: " + edad);
             }
 
         } catch (EOFException e) {
-            System.out.println("Fin del fichero");
+            System.out.println("Fin del Fichero Variable...");
+            try {
+                //Leer fichero fijo
+                System.out.println("Leyendo Fichero Fijo...");
+                DataInputStream ficheroFijo = new DataInputStream(
+                        new FileInputStream("./src/FichDataFija.dat"));
+                while (true) {
+                    String nombreCompleto = "";
+                    for (int i = 1; i <= tamanioNombre; i++) {
+                        caracterNombre = ficheroFijo.readChar();
+                        nombreCompleto = nombreCompleto + caracterNombre;
+                    }
+                    nombreCompleto = nombreCompleto.trim();
+                    int edadFijo = ficheroFijo.readByte();
+                    System.out.println("Nombre: " + nombreCompleto
+                            + ", Edad: " + edadFijo);
+                }
 
+            } catch (EOFException ioe) {
+                System.out.println("Fin Fichero Fijo...");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
