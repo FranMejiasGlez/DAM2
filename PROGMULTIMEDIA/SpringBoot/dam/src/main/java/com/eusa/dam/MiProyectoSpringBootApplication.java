7d31b08070359725dd3f37a525a44bcca2c725ff
fran gonzalez <franciscomejias.24@campuscamara.es>
2026-02-04 23:14:04 +0100

    K

---
+++ PROGMULTIMEDIA/SpringBoot/dam/src/main/java/com/eusa/dam/MiProyectoSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.eusa.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MiProyectoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiProyectoSpringBootApplication.class, args);
+	}
+
+}
+@RestController
+class HelloController {
+@GetMapping("/hello")
+public String sayHello() {
+return "¡Hola, Spring Boot!";
+}
+}
---
+++ PROGMULTIMEDIA/SpringBoot/dam/src/test/java/com/eusa/dam/MiProyectoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eusa.dam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiProyectoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
